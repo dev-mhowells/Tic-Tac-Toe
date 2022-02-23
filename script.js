c9617254f997afb9ef0526a8a7a34e1f21dcad21
Mike <dev.mhowells@gmail.com>
2022-02-23 17:10:50 +0000

    fully functional
    All final features enabled

--- script.js
+++ script.js
@@ -1,11 +1,16 @@
 "use strict";
 
 const container = document.getElementById("container");
-const a1 = document.getElementsByClassName("a1");
+const title = document.getElementById("title");
+const resetButton = document.getElementById("reset-button");
+
+const turnIndicator = document.querySelector(".turn-indicator");
+const player1Indicator = document.querySelector(".big-x");
+const player2Indicator = document.querySelector(".big-o");
 
 const boxes = ["a1", "a2", "a3", "b1", "b2", "b3", "c1", "c2", "c3"];
-const player1List = [];
-const player2List = [];
+let player1List = [];
+let player2List = [];
 const winningConditions = [
   ["a1", "a2", "a3"],
   ["b1", "b2", "b3"],
@@ -17,49 +22,91 @@ const winningConditions = [
   ["a3", "b2", "c1"],
 ];
 
+// WORK ON THIS OR REMOVE IT!
+const clicker = function (playerList) {
+  playerList.push(box.classList[1]);
+  checkForWinner(playerList);
+  checkForDraw();
+};
+
+// MAIN FUNCTIONS ----------------------------------------------------------------
+
 function matches(arr, arr2) {
   return arr.every((i) => arr2.includes(i));
 }
-
-// const endGame = function () {
-//   box.style.backgroundColor = "black";
-// };
 
 const checkForWinner = function (currentPlayer) {
   for (let winCondition of winningConditions) {
     const foundMatch = matches(winCondition, currentPlayer);
     if (foundMatch === true) {
-      // if (player1Active === true) {
-      //   prompt("pink wins");
-      // } else if (player2Active === true) {
-      //   alert("blue wins");
-      // }
-      // endGame();
-      alert("YOU WIN");
+      if (player1Active === true) {
+        title.textContent = "Pink wins";
+      } else if (player2Active === true) {
+        title.textContent = "Blue wins";
+      }
       isWinner = true;
-      break;
     }
   }
 };
 
 const checkForDraw = function () {
   if (player1List.length === 5 && isWinner === false) {
-    alert("DRAW");
+    title.textContent = "Draw";
     isDraw = true;
   }
 };
 
-// STARTING CONDITIONS
+const boxa = document.getElementsByClassName("box");
+
+const playAgain = function () {
+  if (isWinner === true || isDraw === true) {
+    while (container.firstChild) {
+      container.removeChild(container.firstChild);
+    }
+    isDraw = true;
+    player1Active = true;
+    player2Active = false;
+    isWinner = false;
+    isDraw = false;
+    player1Indicator.classList.add("show-active");
+    player2Indicator.classList.remove("show-active");
+    player1List = [];
+    player2List = [];
+    setup();
+  }
+};
+
+const swtitchInidicator = function () {
+  if (player1Active) {
+    player1Indicator.classList.remove("show-active");
+    player2Indicator.classList.add("show-active");
+    player1Active = false;
+    player2Active = true;
+  } else if (player2Active) {
+    player2Indicator.classList.remove("show-active");
+    player1Indicator.classList.add("show-active");
+    player2Active = false;
+    player1Active = true;
+  }
+};
+
+// STARTING CONDITIONS --------------------------
+
 let player1Active = true;
+player1Indicator.classList.add("show-active");
 let player2Active = false;
 let isWinner = false;
 let isDraw = false;
 
-const setup = (function () {
+// GAME FUNCTIONALITY -----------------------------
+
+const setup = function () {
   // CREATE BOARD
+
   for (let n = 0; n < boxes.length; n++) {
     const box = document.createElement("div");
     box.classList.add("box");
+    box.style.backgroundColor = "white";
     box.classList.add(`${boxes[n]}`);
     container.appendChild(box);
 
@@ -70,42 +117,27 @@ const setup = (function () {
         if (isWinner === false && isDraw === false) {
           if (player1Active) {
             box.style.backgroundColor = "pink";
-            // alert("check");
             player1List.push(box.classList[1]);
             checkForWinner(player1List);
             checkForDraw();
-            // console.log(loggedX);
-            player1Active = false;
-            player2Active = true;
+            swtitchInidicator();
           } else if (player2Active) {
             box.style.backgroundColor = "blue";
             player2List.push(box.classList[1]);
             checkForWinner(player2List);
             checkForDraw();
-            // console.log(loggedO);
-            player2Active = false;
-            player1Active = true;
+            swtitchInidicator();
           }
         }
-
-        // console.log(player1Active);
-        // console.log(box.getElementsByClassName);
       },
       { once: true }
     );
   }
-})();
+};
 
-// console.log("testing", loggedX.includes(loggedX.every(boxes)));
-// console.log("testing", loggedX.includes("b"));
-// const includes = function () {
-//   loggedX.includes("a");
-// };
-// console.log(loggedX.every(includes));
+resetButton.addEventListener("click", function () {
+  title.textContent = "Ok";
+  playAgain();
+});
 
-//is everything in a, in b?
-// console.log(matches(loggedO, loggedX));
-
-// if (a1.textContent === "TEST") {
-//   console.log("pink wins");
-// }
+setup();
